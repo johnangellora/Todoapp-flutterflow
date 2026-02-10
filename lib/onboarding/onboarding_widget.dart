41ed1daf1e59419f60e3c7fda6077188378e22b0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-02-10 02:08:51 +0000

    Updating onboarding \'complete profile\' button to a different shade of green

--- lib/onboarding/onboarding_widget.dart
+++ lib/onboarding/onboarding_widget.dart
@@ -607,7 +607,7 @@ class _OnboardingWidgetState extends State<OnboardingWidget> {
                             16.0, 0.0, 16.0, 0.0),
                         iconPadding:
                             EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                        color: FlutterFlowTheme.of(context).primary,
+                        color: FlutterFlowTheme.of(context).success,
                         textStyle:
                             FlutterFlowTheme.of(context).labelMedium.override(
                                   font: GoogleFonts.inter(
